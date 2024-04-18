30c862be514fe3ce711273a381a0acc87251a4b0
Sairam8104 <sairamgoud.devarakadra@gmail.com>
2024-04-18 17:32:22 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { open } = require("sqlite");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const cors = require("cors");
+const dbPath = path.join(__dirname, "student.db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(5000, () => {
+      console.log("Server Running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`Dr Error:${e.message}`);
+  }
+};
+
+initializeDBAndServer();
+
+app.post("/Student/", async (request, response) => {
+  const studentDetails = request.body;
+  const {
+    Name,
+    StudentID,
+    StudentYear,
+    StudentCourse,
+    Address,
+    State,
+  } = studentDetails;
+  const createNewStudent = `INSERT INTO student (Name,StudentID,StudentYear,StudentCourse,Address,State)
+  values(
+    "${Name}","${StudentID}","${StudentYear}","${StudentCourse}","${Address}","${State}"
+  );`;
+  await db.run(createNewStudent);
+  response.send("Student Successfully Added");
+});
+
+app.get("/student/", async (request, response) => {
+  const getMovieName = `SELECT *  FROM student;`;
+  const movieNameArray = await db.all(getMovieName);
+  response.send(movieNameArray);
+});
+module.exports = app;
